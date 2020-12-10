dcd90e2bca3f59b9d6f39ddebb80faf1d165f90b
James Allred <wildj79@gmail.com>
2020-12-10 09:01:53 -0600

    Initial commit.

---
+++ src/echo.js
@@ -0,0 +1,47 @@
+/**
+ * This is a simple utilty that echo's what you type into a new ChatMessage.
+ * The magic of this module is that it will evaluate any roll-like parameters that you
+ * pass it.
+ * 
+ * @author James Allred
+ * @license MIT
+ * @copyright (c) 2020 James Allred
+ */
+
+// Import JavaScript modules
+import { registerSettings } from './module/settings.js';
+import { preloadTemplates } from './module/preloadTemplates.js';
+
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
+Hooks.once('init', async function() {
+	console.log('echo | Initializing echo');
+
+	// Assign custom classes and constants here
+	
+	// Register custom module settings
+	registerSettings();
+	
+	// Preload Handlebars templates
+	await preloadTemplates();
+
+	// Register custom sheets (if any)
+});
+
+/* ------------------------------------ */
+/* Setup module							*/
+/* ------------------------------------ */
+Hooks.once('setup', function() {
+	// Do anything after initialization but before
+	// ready
+});
+
+/* ------------------------------------ */
+/* When ready							*/
+/* ------------------------------------ */
+Hooks.once('ready', function() {
+	// Do anything once the module is ready
+});
+
+// Add any additional hooks if necessary
